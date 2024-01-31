4af16ff27f29df1bb138439bf4b83f07f6fc7848
Caleb Hemphill <calebhemp@gmail.com>
2024-01-31 18:34:51 -0500

    Add pytest to tests

--- tests/test_caesar.py
+++ tests/test_caesar.py
@@ -1,45 +1,44 @@
+import pytest
 from caesar_cipher.cipher import encrypt, decrypt, crack
 
-"""
-place in your projects tests folder
-"""
 
+# @pytest.mark.skip()
 def test_encrypt_shift_1():
     actual = encrypt("apple", 1)
     expected = "bqqmf"
     assert actual == expected
 
-
+# @pytest.mark.skip()
 def test_encrypt_shift_10():
     actual = encrypt("apple", 10)
     expected = "kzzvo"
     assert actual == expected
 
-
+# @pytest.mark.skip()
 def test_encrypt_shift_20():
     actual = encrypt("apple", 20)
     expected = "ujjfy"
     assert actual == expected
 
-
+# @pytest.mark.skip()
 def test_uppercase():
     actual = encrypt("BANANA", 10)
     expected = "LKXKXK"
     assert actual == expected
 
-
+# @pytest.mark.skip()
 def test_with_whitespace():
     actual = encrypt("apples and bananas", 1)
     expected = "bqqmft boe cbobobt"
     assert actual == expected
 
-
+# @pytest.mark.skip()
 def test_with_non_alpha():
     actual = encrypt("Gimme a 1!", 1)
     expected = "Hjnnf b 1!"
     assert actual == expected
 
-
+# @pytest.mark.skip()
 def test_round_trip():
     original = "Gimme a 1!"
     shift = 5
@@ -48,7 +47,7 @@ def test_round_trip():
     expected = original
     assert actual == expected
 
-
+@pytest.mark.skip()
 def test_crack_phrase():
     phrase = "It was the best of times, it was the worst of times."
     encrypted = encrypt(phrase, 10)
@@ -56,7 +55,7 @@ def test_crack_phrase():
     expected = phrase
     assert actual == expected
 
-
+@pytest.mark.skip()
 def test_crack_nonsense():
     phrase = "Ix fhw txe fofg of ndhrl, it nad tho hndrk of allkd."
     encrypted = encrypt(phrase, 10)
