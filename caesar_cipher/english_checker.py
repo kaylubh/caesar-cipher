056feabd62cfa62abf83fac6fcc3adb3a57593f6
Caleb Hemphill <calebhemp@gmail.com>
2024-01-31 19:25:20 -0500

    Add function to count english words

--- caesar_cipher/english_checker.py
+++ caesar_cipher/english_checker.py
@@ -1,1 +1,30 @@
-from corpus import word_list, name_list
+from caesar_cipher.corpus import word_list, name_list
+
+
+def count_english_words(text):
+    """
+    
+    """
+
+    candidate_words = []
+    english_words_count = 0
+
+    # remove all characters that are not letters
+    for word in text.split(): 
+
+        candidate_word = ""
+
+        for char in word:
+            
+            if char.isalpha():
+                candidate_word += char
+
+        candidate_words.append(candidate_word)
+
+    # check candidate word against english corpus
+    for word in candidate_words:
+
+        if word.lower() in word_list or word in name_list:
+            english_words_count += 1
+
+    return english_words_count
